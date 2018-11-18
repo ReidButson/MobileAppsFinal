0321edff05c029b7f40efb5bf90eb0142f86bfc5
GuyMcFail <34040408+GuyMcFail@users.noreply.github.com>
2018-11-18 17:01:51 -0500

    Create Recipe Basic Functions
    TODO populate list from database

--- app/src/main/java/com/mobileapps/uoit/receipy/Homepage.java
+++ app/src/main/java/com/mobileapps/uoit/receipy/Homepage.java
@@ -1,7 +1,10 @@
 package com.mobileapps.uoit.receipy;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Homepage extends AppCompatActivity {
 
@@ -9,5 +12,19 @@ public class Homepage extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_homepage);
+
+        recipesConifgure();
+    }
+
+    void recipesConifgure(){
+        Button recipes_btn = findViewById(R.id.recipe_list_btn);
+
+        recipes_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent toRecipes = new Intent(Homepage.this, RecipeList.class);
+                startActivity(toRecipes);
+            }
+        });
     }
 }
--- app/src/main/java/com/mobileapps/uoit/receipy/RecipeList.java
+++ app/src/main/java/com/mobileapps/uoit/receipy/RecipeList.java
@@ -1,7 +1,15 @@
 package com.mobileapps.uoit.receipy;
 
+import android.content.Context;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 public class RecipeList extends AppCompatActivity {
 
@@ -9,5 +17,39 @@ public class RecipeList extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_recipe_list);
+
+        addRecipeConfigure();
+    }
+
+    @Override
+    protected void onActivityResult(int request_code, int result_code, Intent data){
+        if (request_code == 1){
+            final LayoutInflater inflater = (LayoutInflater) getApplicationContext()
+                    .getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+
+            final LinearLayout recipe_holder = findViewById(R.id.recipe_holder);
+
+            String name = data.getStringExtra("RECIPE_NAME");
+
+            View recipe = inflater.inflate(R.layout.recipe_field, null);
+
+            TextView name_field =  recipe.findViewById(R.id.recipe_name_txt);
+            
+            name_field.setText(name);
+
+            recipe_holder.addView(recipe, recipe_holder.getChildCount() - 1);
+        }
+    }
+
+    void addRecipeConfigure(){
+        Button recipe_btn = findViewById(R.id.add_recipe_btn);
+
+        recipe_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent create_recipe = new Intent(RecipeList.this, CreateRecipe.class);
+                startActivityForResult(create_recipe, 1);
+            }
+        });
     }
 }
