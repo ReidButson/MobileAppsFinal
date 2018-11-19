32808546e5ad477583a74c541b75715418cf76df
GuyMcFail <34040408+GuyMcFail@users.noreply.github.com>
2018-11-19 17:20:06 -0500

    Delete Things
    things can be deleted

--- app/src/main/java/com/mobileapps/uoit/receipy/RecipeList.java
+++ app/src/main/java/com/mobileapps/uoit/receipy/RecipeList.java
@@ -2,22 +2,30 @@ package com.mobileapps.uoit.receipy;
 
 import android.content.Context;
 import android.content.Intent;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
-import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.CheckBox;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
+import java.util.ArrayList;
+
 public class RecipeList extends AppCompatActivity {
+
+    private LinearLayout recipe_holder;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_recipe_list);
 
+        recipe_holder = findViewById(R.id.recipe_holder);
+
+        deleteChecked();
         addRecipeConfigure();
     }
 
@@ -27,11 +35,17 @@ public class RecipeList extends AppCompatActivity {
             final LayoutInflater inflater = (LayoutInflater) getApplicationContext()
                     .getSystemService(Context.LAYOUT_INFLATER_SERVICE);
 
-            final LinearLayout recipe_holder = findViewById(R.id.recipe_holder);
-
             String name = data.getStringExtra("RECIPE_NAME");
 
-            View recipe = inflater.inflate(R.layout.recipe_field, null);
+            final View recipe = inflater.inflate(R.layout.recipe_field, null);
+
+            recipe.setOnLongClickListener(new View.OnLongClickListener() {
+                @Override
+                public boolean onLongClick(View v) {
+                    revealChecks();
+                    return false;
+                }
+            });
 
             TextView name_field =  recipe.findViewById(R.id.recipe_name_txt);
 
@@ -39,6 +53,51 @@ public class RecipeList extends AppCompatActivity {
 
             recipe_holder.addView(recipe, recipe_holder.getChildCount() - 1);
         }
+    }
+
+    public void deleteChecked(){
+        final FloatingActionButton delete_flt_btn = findViewById(R.id.floatingActionButton);
+
+        delete_flt_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ArrayList<View> blacklisted = new ArrayList<>();
+                for (int i = 0; i < recipe_holder.getChildCount(); i++){
+                    View recipe_panel = recipe_holder.getChildAt(i);
+                    CheckBox check = recipe_panel.findViewById(R.id.delete_chkbx);
+                    if (check.isChecked()){
+                        blacklisted.add(recipe_panel);
+                    }
+                }
+                for(View x : blacklisted){
+                    delete_recipe(x);
+                }
+                hideChecks();
+            }
+        });
+    }
+
+    public void revealChecks() {
+        findViewById(R.id.floatingActionButton).setVisibility(View.VISIBLE);
+        for (int i = 0; i < recipe_holder.getChildCount(); i++) {
+            View recipe_panel = recipe_holder.getChildAt(i);
+            CheckBox check = recipe_panel.findViewById(R.id.delete_chkbx);
+            check.setVisibility(View.VISIBLE);
+        }
+    }
+
+    public void hideChecks() {
+        findViewById(R.id.floatingActionButton).setVisibility(View.VISIBLE);
+        for (int i = 0; i < recipe_holder.getChildCount(); i++) {
+            View recipe_panel = recipe_holder.getChildAt(i);
+            CheckBox check = recipe_panel.findViewById(R.id.delete_chkbx);
+            check.setVisibility(View.INVISIBLE);
+        }
+        findViewById(R.id.floatingActionButton).setVisibility(View.INVISIBLE);
+    }
+
+    public void delete_recipe(View v){
+        recipe_holder.removeView(v);
     }
 
     void addRecipeConfigure(){
